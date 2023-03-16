4f224ea78fb0ba1d2cff860df403575dfb484373
781611228@qq.com <xtz>
2023-03-16 17:28:40 +0800

    第二次commit

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,19 +1,22 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.base.exception.ValidationGroups;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @description TODO
+ * @description 课程信息管理
  * @author Mr.M
  * @date 2022/10/7 16:22
  * @version 1.0
@@ -33,5 +36,38 @@ public class CourseBaseInfoController {
 
         return  courseBasePageResult;
     }
+
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated(ValidationGroups.Inster.class) AddCourseDto addCourseDto){
+
+        Long companyId = 22L;
+        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
+    }
+
+
+    @GetMapping("/course/{courseId}")
+    public CourseBaseInfoDto getCourseBaseById(@PathVariable Long courseId){
+
+
+        return courseBaseInfoService.getCourseBaseInfo(courseId);
+    }
+
+    @PutMapping("/course")
+    public CourseBaseInfoDto modifyCourseBase(@RequestBody EditCourseDto dto){
+
+        Long companyId =1232141425L;
+        return courseBaseInfoService.updateCourseBase(companyId,dto);
+    }
+
+
+    //测试分级校验用
+//    @PostMapping("/course2")
+//    public CourseBaseInfoDto createCourseBase2(@RequestBody @Validated(ValidationGroups.Update.class) AddCourseDto addCourseDto){
+//
+//        Long companyId = 22L;
+//        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
+//    }
+
+
 
 }
